e763f944ed2e96b146088844757166309b338e27
wyatt4543 <w4543w@gmail.com>
2026-04-22 12:00:39 -0500

    fix the reference to the css file

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Woogi World - Log in to WoogiWorld</title>
-    <link rel="stylesheet" href="css/login-styles.css">
+    <link rel="stylesheet" href="../css/login-styles.css">
 </head>
 <body>
     <script type="text/javascript" async="" src="login.js"></script>
