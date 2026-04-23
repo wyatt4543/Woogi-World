263b846f0177fd57c9db6d0aee107bb5afc54634
wyatt4543 <w4543w@gmail.com>
2026-04-23 00:04:17 -0500

    create code to update the square's position upon click

--- go.js
+++ go.js
@@ -12,6 +12,12 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.canvas.addEventListener("click", function(event) {
+            let x = event.offsetX;
+            let y = event.offsetY;
+
+            myGamePiece.newPos(x, y);
+        });
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
     },
@@ -37,16 +43,15 @@ function component(width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function () {
-        this.x = 120;
-        this.y = 100;
+    this.newPos = function (x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
 function updateGameArea() {
     myGameArea.clear();
     myGameArea.frameNo += 1;
-    myGamePiece.newPos();
     myGamePiece.update();
 }
 
