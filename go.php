62f8d2b4cb59192aa0d40480c0584ae73705a40c
wyatt4543 <w4543w@gmail.com>
2026-04-22 23:24:15 -0500

    create the basic html for the game page

--- go.php
+++ go.php
@@ -2,3 +2,15 @@
 session_start();
 echo "username: {$_SESSION['username']}";
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woogi World - Play</title>
+</head>
+<body>
+</body>
+</html>
